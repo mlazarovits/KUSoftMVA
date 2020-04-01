c6f32b57fb1373e88ae3d0ff2c440ab6ac02b851
Margaret Lazarovits <mlazarovits@gmail.com>
2020-04-01 17:02:15 -0400

    check how tree is being filled

--- test/makeTrees.C
+++ test/makeTrees.C
@@ -28,7 +28,7 @@ void makeTrees(selectortype& selector, string ofilename){
 	auto muonTree = selector.fChain->CloneTree(0);
 	for(int i = 0;i<selector.fChain->GetEntries();i++){
 		selector.fChain->GetEntry(i);
-		if(i < 10) cout << *selector.GenPart_statusFlags << endl;
+		if(i < 10) cout << *selector.nMuon << endl;
 		muonTree->Fill();
 	}
 	muonTree->Write();
