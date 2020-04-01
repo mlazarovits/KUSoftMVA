1b57fc800dd894e8a24d9752740d56551b6c45c5
Margaret Lazarovits <mlazarovits@gmail.com>
2020-04-01 17:40:18 -0400

    added checkMother macro

--- test/checkMother.C
+++ test/checkMother.C
@@ -2,13 +2,13 @@ void checkMother(TFile* file){
 	TTree* tree = (TTree*)file->Get("Events");
 	int nEntries = tree->GetEntries();
 
-	for(i = 0;i < nEntries; i++){
+	for(int i = 0;i < nEntries; i++){
 		tree->GetEntry(i);
 		// if(tree->GetLeaf("nMuon")->GetValue == 0) continue;
-		int nGenPart = tree->GetLeaf("nGenPart");
+		int nGenPart = tree->GetLeaf("nGenPart")->GetValue();
 		
 
-		for(j = 0; j < nGenPart; j++){
+		for(int j = 0; j < nGenPart; j++){
 			if(tree->GetLeaf("GenPart_pdgId")->GetValue(j) == 13 || tree->GetLeaf("GenPart_pdgId")->GetValue(j) == -13){
 				cout << "Generator muon mother id: " << tree->GetLeaf("GenPart_genPartIdxMother")->GetValue(j) << endl;
 			}
