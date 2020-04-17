0e4d49e5da1cce67a00ba929bc795f9649e8b309
Margaret Lazarovits <mlazarovits@gmail.com>
2020-04-17 17:54:25 -0500

    updated pt binning and debug graph graphics

--- MuonAnalysis/interface/SoftIdEfficiency.h
+++ MuonAnalysis/interface/SoftIdEfficiency.h
@@ -247,24 +247,24 @@ inline std::vector<Double_t> SoftIdEfficiency::makeEffBins(TString inputvar){
 	if(strstr(inputvar, "pt")){
 		nBins = 20;
 		effbins.push_back(0.0);
-		// for(int i = 0; i < 19; i++){
-		// 	effbins.push_back(effbins.at(i-1) + 1.0);
-		// }
-		//SOS binning
-		for(int i = 1; i < 2; i++){
-			effbins.push_back(effbins.at(i-1) + 4.0);
-			// cout << effbins[i] << endl;
-		}
-		for(int i = 2; i < 5; i++){
+		for(int i = 1; i < 20; i++){
 			effbins.push_back(effbins.at(i-1) + 1.0);
-			// cout << effbins[i] << endl;
 		}
-		for(int i = 5; i < 6; i++){
-			effbins.push_back(effbins.at(i-1) + 3.);
-		}
-		for(int i = 6; i < 11; i++){
-			effbins.push_back(effbins.at(i-1) + 5.);
-		}
+		//SOS binning
+		// for(int i = 1; i < 2; i++){
+		// 	effbins.push_back(effbins.at(i-1) + 4.0);
+		// 	// cout << effbins[i] << endl;
+		// }
+		// for(int i = 2; i < 5; i++){
+		// 	effbins.push_back(effbins.at(i-1) + 1.0);
+		// 	// cout << effbins[i] << endl;
+		// }
+		// for(int i = 5; i < 6; i++){
+		// 	effbins.push_back(effbins.at(i-1) + 3.);
+		// }
+		// for(int i = 6; i < 11; i++){
+		// 	effbins.push_back(effbins.at(i-1) + 5.);
+		// }
 	}
 	else if(strstr(inputvar, "eta")){
 
@@ -559,7 +559,7 @@ inline void SoftIdEfficiency::makePlots(vector<TEfficiency*> effs){
 	// gr_effs[imax]->Draw();
 
 	cv->Update();
-	Int_t chopcolor = gr_effs.size()/3;
+	Int_t chopcolor = gr_effs.size()/2;
 	Int_t chopmarker = gr_effs.size()/3;
 
 	for(int i = 0; i < gr_effs.size(); i++){
