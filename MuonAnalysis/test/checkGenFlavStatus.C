ba9c9d324d65b0865276a0c1e6e6be0106d39311
Margaret Lazarovits <mlazarovits@gmail.com>
2020-04-24 14:08:08 -0500

    debug

--- MuonAnalysis/test/checkGenFlavStatus.C
+++ MuonAnalysis/test/checkGenFlavStatus.C
@@ -3,7 +3,7 @@ void checkGenFlavStatus(){
 	TTree* tree = (TTree*)file->Get("Events");
 	int nEntries = tree->GetEntries();
 
-	TH1F* dR_hist = new TH1F("dR_hist","dR_hist",100,0,5.);
+	TH1F* dR_hist = new TH1F("dR_hist","dR_hist",100,0,10);
 	// TH1F* genIdx_hist = new TH1F("genIdx_hist","genIdx_hist",100,-50,50);
 	TH2F* dRdPt_hist = new TH2F("dRdPt_hist","dRdPt_hist",25,0,0.6,25,0,1);
 
@@ -78,7 +78,7 @@ void checkGenFlavStatus(){
 	cout << "# dR entries: " << dR_hist->Integral() << endl;
 	cout << "# dRdPt_hist entries: " << dRdPt_hist->Integral() << endl; 
 	TCanvas* cv = new TCanvas();
-	TCanvas* cv2 = new TCanvas();
+	// TCanvas* cv2 = new TCanvas();
 
 	cv->cd();
 	// dRdPt_hist->GetYaxis()->SetTitle("delta pT rel");
