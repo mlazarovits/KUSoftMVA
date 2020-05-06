793e428076451ed7c53b41997aa07c15f5207a20
Margaret Lazarovits <mlazarovits@gmail.com>
2020-05-05 21:09:46 -0500

    still working with softID, increased batch size to 256

--- MuonAnalysis/test/NN_part1.py
+++ MuonAnalysis/test/NN_part1.py
@@ -143,7 +143,7 @@ model.compile(loss='categorical_crossentropy',optimizer=Adam(),metrics=['accurac
 # model.summary()
 
 
-model.fit(x_train,y_train,batch_size=128,epochs=5)
+model.fit(x_train,y_train,batch_size=256,epochs=5)
 
 
 
