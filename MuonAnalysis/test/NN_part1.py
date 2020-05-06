bba84873f275920d2a96d8ad1c11e5e00dbbe234
Margaret Lazarovits <mlazarovits@gmail.com>
2020-05-05 21:07:23 -0500

    switched back to softID to train, added Dense layer of 64 neurons - now at 4 layers

--- MuonAnalysis/test/NN_part1.py
+++ MuonAnalysis/test/NN_part1.py
@@ -134,6 +134,7 @@ inputs = Input(shape=x_train[0].shape)
 x = Dense(64,activation='relu')(inputs)
 x = Dense(64,activation='relu')(x)
 x = Dense(64,activation='relu')(x)
+x = Dense(64,activation='relu')(x)
 outputs = Dense(nClasses,activation='softmax')(x)
 
 model = Model(inputs=inputs,outputs=outputs)
