801d22f6b8a2fc02f2e8deb92f33999879077f1a
Margaret Lazarovits <mlazarovits@gmail.com>
2020-05-10 13:53:29 -0500

    tchain debug

--- MuonAnalysis/plugins/Plot_Efficiency.C
+++ MuonAnalysis/plugins/Plot_Efficiency.C
@@ -20,14 +20,14 @@ void Plot_Efficiency(TString sampleName){
 	}
 
 	
-	string gPathname = "home/t3-ku/mlazarov/softMVA/CMSSW_10_6_11_patch1/src/KUSoftMVA/MuonAnalysis/test/";
+	string gPathname = "/home/t3-ku/mlazarov/softMVA/CMSSW_10_6_11_patch1/src/KUSoftMVA/MuonAnalysis/test/";
 	TFile* fTTJets = TFile::Open((gPathname+"OutputFiles/TTJets2018_MINI_numEvent100000.root").c_str());
-	TFile* fQCD = TFile::Open((gPathname+"OutputFiles/QCD_pt_600to800_2018_MINI_numEvent100000.root").c_str());;
-	TFile* fDYJets = TFile::Open((gPathname+"OutputFiles/DYJetsToLL2018_MINI_numEvent100000.root").c_str());;
+	TFile* fQCD = TFile::Open((gPathname+"OutputFiles/QCD_pt_600to800_2018_MINI_numEvent100000.root").c_str());
+	TFile* fDYJets = TFile::Open((gPathname+"OutputFiles/DYJetsToLL2018_MINI_numEvent100000.root").c_str());
 	TChain* allFiles = new TChain();
-	allFiles->AddFile(fTTJets);
-	allFiles->AddFile(fQCD);
-	allFiles->AddFile(fDYJets);
+	allFiles->Add((gPathname+"OutputFiles/TTJets2018_MINI_numEvent100000.root").c_str());
+	allFiles->Add((gPathname+"OutputFiles/QCD_pt_600to800_2018_MINI_numEvent100000.root").c_str());
+	allFiles->Add((gPathname+"OutputFiles/DYJetsToLL2018_MINI_numEvent100000.root").c_str());
 
 
 if(sampleName=="TTJets"){
