86c89e158e78eada1d1e9716bc87f1c7f74af09e
Margaret Lazarovits <mlazarovits@gmail.com>
2020-05-09 21:46:05 -0500

    update efficiency plot aesthetics - debug

--- MuonAnalysis/test/margaret_work/plotFunctions.py
+++ MuonAnalysis/test/margaret_work/plotFunctions.py
@@ -185,13 +185,13 @@ def plotEfficiency(effs,outName,outFile):
 	cv.Update()
 
 	gr_effs.append(effs[0].GetPaintedGraph())
-	for i in range(1, len(effs):
+	for i in range(1, len(effs)):
 		effs[i].Draw("same")
 		cv.Update()
 		gr_effs.append(effs[i].GetPaintedGraph())
 	cv.Update()
 
-	
+
 	# chopcolor = len(gr_effs)/1
 	# chopmarker = len(gr_effs)/3
 
