513b5109036d40330a0db7f1f51285adb2f3ebe1
Margaret Lazarovits <mlazarovits@gmail.com>
2020-05-10 20:52:27 -0500

    added debugs from PR #7 from JPhsx

--- MuonAnalysis/test/margaret_work/mva/NN.py
+++ MuonAnalysis/test/margaret_work/mva/NN.py
@@ -8,7 +8,7 @@ from keras.models import Sequential, Model
 from keras.layers import *
 from keras.optimizers import SGD, Adam
 from keras.activations import relu
-# np.set_printoptions(threshold=sys.maxsize)
+np.set_printoptions(threshold=sys.maxsize)
 from keras import layers
 from sklearn.utils import shuffle
 from array import array
@@ -29,77 +29,73 @@ def evaluateModel(model_y, true_y, model_pt, fname, tag, nClasses,mdict, results
 	pred[ pred >= 0.5 ] = 1.
 	pred[ pred <0.5 ] = 0.
 
-	h_num = [ TH1D("num_"+str(i), "label "+str(i), 41, -0.5, 40.5 ) for i in range(nClasses) ]	
+	#this num is model predicts class and object matched to same class
+	h_num = [ TH1D("num_"+str(i), "label "+str(i), 41, -0.5, 40.5 ) for i in range(nClasses) ]
+
+	#den of objects of a specific class
 	h_den = [ TH1D("den_"+str(i), "label "+str(i), 41, -0.5, 40.5 ) for i in range(nClasses) ]
 
 	num_ctr = [ 0. for i in range(nClasses) ]
 	den_ctr = [ 0. for i in range(nClasses) ]
 
-	all_num_ctr =  0. 
-	all_den_ctr =  0. 
+	all_fnum_ctr = 0
+    all_fden_ctr = 0
 
-	h_fnum = [ TH1D("fnum_"+str(i), "correct id label "+str(i), 41, -0.5, 40.5 ) for i in range(nClasses) ]	
-	#h_fden = [ TH1D("fden_"+str(i), "mis id. label "+str(i), 41, -0.5, 40.5 ) for i in range(nClasses) ]
+	h_fnum = [ TH1D("fnum_"+str(i), "correct id label "+str(i), 41, -0.5, 40.5 ) for i in range(nClasses) ]
+
+	#den of all objects that are labeled by model
+	h_fden = [ TH1D("fden_"+str(i), "model predicts label "+str(i), 41, -0.5, 40.5 ) for i in range(nClasses) ]
 
 	fnum_ctr = [ 0 for i in range(nClasses) ]
-	#fden_ctr = [ 0 for i in range(nClasses) ]
+	fden_ctr = [ 0 for i in range(nClasses) ]
 
 	all_fnum_ctr = 0
-	#all_fden_ctr = 0
+	all_fden_ctr = 0
 
-	# begin the counting 
-	for i , (my, ty, pt) in enumerate(zip(pred, true_y, model_pt)):
-		#print i,my,ty,pt
-		labelidx = -1
-		for idx in range(len(ty)):
-			if( ty[idx] == 1):
-				labelidx = idx
-		if( (my == ty).all() ): #we have a correct classification
-			h_num[labelidx].Fill(pt)
-			h_den[labelidx].Fill(pt)
-			num_ctr[labelidx] = num_ctr[labelidx] + 1.
-			den_ctr[labelidx] = num_ctr[labelidx] + 1.
-			
+    # begin the counting 
+    for i , (my, ty, pt) in enumerate(zip(pred, true_y, model_pt)):
+            #print i,my,ty,pt
+            labelidx = -1
+            modelidx = -1
+            for idx in range(len(ty)):
+                    if( ty[idx] == 1):
+                            labelidx = idx
+            for idx in range(len(my)):
+                    if( my[idx] ==1):
+                            modelidx = idx
+             if( (my == ty).all() ): #we have a correct classification
+                    h_num[labelidx].Fill(pt)
+                    h_fnum[labelidx].Fill(pt)
 
-			all_num_ctr = all_num_ctr + 1.
-			all_den_ctr = all_den_ctr + 1.
-		
-		else: #incorrect classification
-			#h_fnum	
-			h_fnum[labelidx].Fill(pt)
-			h_den[labelidx].Fill(pt)
-			fnum_ctr[labelidx] = fnum_ctr[labelidx] + 1.
-			den_ctr[labelidx] = den_ctr[labelidx] + 1.
-			
-			all_fnum_ctr = all_fnum_ctr +1.
-			all_den_ctr = all_den_ctr + 1.
+                    num_ctr[labelidx] = num_ctr[labelidx] + 1.
 
-		#collect all classifications as well
+                    all_num_ctr = all_num_ctr + 1.
+            #       all_fnum_ctr = all_fnum_ctr + 1.
 
-			
-	print("Reporting results from test data")
-	for x in range(nClasses):
-		if den_ctr[x] == 0:
-			perc = 0.
-			fperc = 0.
-		else:
-			perc = num_ctr[x]/den_ctr[x]
-			fperc = fnum_ctr[x]/den_ctr[x]
-	
-		print("label "+str(x)+":")
-		print("Correct ID: "+ str(num_ctr[x]) +" of "+ str(den_ctr[x])+"   "+str(perc))
-		print("Mis.    ID: "+ str(fnum_ctr[x])+" of "+ str(den_ctr[x])+"   "+str(fperc))
+            h_den[labelidx].Fill(pt)
+            h_fden[modelidx].Fill(pt)
 
-	if all_den_ctr == 0:
-		allperc = 0.
-		allfperc = 0.
-	else:
-		allperc = all_num_ctr/all_den_ctr
-		allfperc = all_fnum_ctr/all_den_ctr
-		
-	print("Overall performance: ")
-	print("Correct ID: "+ str(all_num_ctr)+" of "+ str( all_den_ctr)+"   "+str(allperc))
-	print("Mis.    ID: "+ str(all_fnum_ctr)+" of "+ str( all_den_ctr)+"   "+str(allfperc))
+            den_ctr[labelidx] = den_ctr[labelidx] + 1.
+            fden_ctr[modelidx] = fden_ctr[modelidx] + 1.
+
+            all_den_ctr = all_den_ctr + 1.
+
+
+		print("Reporting results from test data")
+		print("We define efficiency and purity s.t.")
+		print("efficiency = # of objects correctly classified for a specific label / # of true objects of that label")
+		print("purity = # of objects correctly classified for a specific label/ # of objects classified for that label")
+        for x in range(nClasses):
+            print("label "+str(x)+":")
+            print("Efficiency : "+ str(num_ctr[x]) +" of "+ str(den_ctr[x])+"   "+str(num_ctr[x]/den_ctr[x]))
+            print("Purity     : "+ str(num_ctr[x])+" of "+ str(fden_ctr[x])+"   "+str(num_ctr[x]/fden_ctr[x]))
+
+        print("Overall performance: ")
+       # print("Correct ID: "+ str(all_num_ctr)+" of "+ str( all_den_ctr)+"   "+str(all_num_ctr/all_den_ctr))
+        #print("Mis.    ID: "+ str(all_fnum_ctr)+" of "+ str( all_den_ctr)+"   "+str(all_fnum_ctr/all_den_ctr))
+        print("Purity: "+ str(all_num_ctr)+" of "+str(all_den_ctr)+"    "+str(all_num_ctr/all_den_ctr))
+
+
 	
 
 
@@ -107,16 +103,14 @@ def evaluateModel(model_y, true_y, model_pt, fname, tag, nClasses,mdict, results
 
 	goodEff = [ TEfficiency(h_num[i],h_den[i]) for i in range(nClasses) ]
 	[ goodEff[i].SetName("correct"+str(i)) for i in range(nClasses) ]
-	badEff = [ TEfficiency(h_fnum[i],h_den[i]) for i in range(nClasses) ]
-	[ badEff[i].SetName("fake"+str(i)) for i in range(nClasses) ]
+	badEff = [ TEfficiency(h_fnum[i],h_fden[i]) for i in range(nClasses) ]
+	[ badEff[i].SetName("pure"+str(i)) for i in range(nClasses) ]
 
 #[ self.tr_acc, self.tr_loss, self.tr_valacc, self.tr_valloss]
- 
-	h_loss = TGraph(  len(results[1]), array('d',results[1]), array('d',list(np.arange(len(results[1])))) )
-	h_vloss= TGraph(  len(results[3]), array('d',results[3]), array('d',list(np.arange(len(results[3])))) )
-	h_acc = TGraph(	  len(results[0]), array('d',results[0]), array('d',list(np.arange(len(results[0])))) )
-	h_vacc = TGraph(  len(results[2]), array('d',results[2]), array('d',list(np.arange(len(results[2])))) )
-
+	h_loss = TGraph(  len(results[1]),  array('d',list(np.arange(len(results[1])))), array('d',results[1])   )
+	h_vloss= TGraph(  len(results[3]),  array('d',list(np.arange(len(results[3])))), array('d',results[3])   )
+	h_acc = TGraph(   len(results[0]),  array('d',list(np.arange(len(results[0])))), array('d',results[0])   )
+	h_vacc = TGraph(  len(results[2]),  array('d',list(np.arange(len(results[2])))), array('d',results[2])   )
 #	c1 = TCanvas()
 #	goodEff[0].Draw()
 	
