7c0778ad9db371058c085c961f0c7780dac30c80
Margaret Lazarovits <mlazarovits@gmail.com>
2020-07-02 14:47:02 -0500

    test chunking methods in uproot

--- MuonAnalysis/python/DATA.py
+++ MuonAnalysis/python/DATA.py
@@ -25,44 +25,42 @@ def dropBenchmarkVar( sample ):#, benchvars ):
 	newsamp = newsamp.drop(columns='Muon_softMvaId')
 	return newsamp
 
-def prepareBenchSet( sample, model_vars, label_dict):
-	data=  sample[ model_vars ]
+def prepareBenchSet( data, target):
+	# data=  sample[ model_vars ]
 	
-	target = data['Muon_genPdgId']
-	target=abs(target)
+	# target = data['Muon_genPdgId']
+	# target=abs(target)
 	
-	data = data.drop(columns='Muon_genPdgId')
-	pt = data['Muon_pt']
-	target= target.map(label_dict)
-	data = (data-data.mean())/data.std()
-	x_train,x_test, y_train, y_test = train_test_split(data,target, test_size=.99, random_state=1)
-	pt_train,pt_test, y_train2, y_test2= train_test_split(pt,target, test_size=.99, random_state=1)
-	x_train = x_train.to_numpy()
+	# data = data.drop(columns='Muon_genPdgId')
+	# pt = data['Muon_pt']
+	# target= target.map(label_dict)
+	# data = (data-data.mean())/data.std()
+
+	_,x_test, _, y_test = train_test_split(data,target, test_size=.99, random_state=1)
+	_,pt_test, _, _= train_test_split(pt,target, test_size=.99, random_state=1)
+	
 	x_test = x_test.to_numpy()
-	y_train = y_train.to_numpy()
 	y_test = y_test.to_numpy()
 	pt_test = pt_test.to_numpy()
 	return x_test,y_test,pt_test
 
-def prepareTrainingSet( sample , model_vars, label_dict ):
-	data = sample[ model_vars ]
-#	print(data)
-	data = shuffle(data)
-	target = data['Muon_genPdgId']
-	target = abs(target)
-	data = data.drop(columns='Muon_genPdgId')
+def prepareTrainingSet( data, target ):
+# 	data = sample[ model_vars ]
+# #	print(data)
+# 	data = shuffle(data)
+# 	target = data['Muon_genPdgId']
+# 	target = abs(target)
+# 	data = data.drop(columns='Muon_genPdgId')
 
 	
-	pt = data['Muon_pt']
-#	data = data.drop(columns='Muon_pt')
+# 	pt = data['Muon_pt']
+# #	data = data.drop(columns='Muon_pt')
 
-	target = target.map(label_dict)
-	data = (data-data.mean())/data.std()	
+# 	target = target.map(label_dict)
+# 	data = (data-data.mean())/data.std()	
 	
 	x_train, x_test, y_train, y_test = train_test_split(data, target, test_size = .35, random_state=1)
 	pt_train, pt_test, y_train2, y_test2 = train_test_split(pt,target, test_size= .35, random_state=1)
-
-
 	x_train = x_train.to_numpy()
 	x_test = x_test.to_numpy()
 	y_train = y_train.to_numpy()
@@ -75,6 +73,8 @@ def prepareTrainingSet( sample , model_vars, label_dict ):
 
 	return x_train, x_test, y_train, y_test, pt_train, pt_test
 
+
+	
 
 
 def reportSample( sample):
@@ -94,35 +94,66 @@ def reportSample( sample):
 
 
 
+def preprocessing(sample , model_vars, label_dict):
+	events = uproot.open(self.fname)[self.treeName]
+	memChunks = [i for i in events.mempartitions(1e6)]
+	pdgIds = np.array([])
+	for i in memChunks:
+		memStart = i[0]
+		memStop = i[1]
+		data = events.arrays(model_vars,entrystart=memStart,entrystop=memStop)
 
+		#make labels
+		genData = events.array('GenPart_pdgId',entrystart=memStart,entrystop=memStop)
+		dataMu = events.array('Muon_genPartIdx',entrystart=memStart,entrystop=memStop)
+		pdgIds = np.append(pdgIds,[-999 if mu == -1 else genData[i][mu] for i, idxs in enumerate(dataMu) for j, mu in enumerate(idxs)])
+			
+
+
+
+	data = sample[ model_vars ]
+#	print(data)
+	data = shuffle(data)
+	target = data['Muon_genPdgId']
+	target = abs(target)
+	data = data.drop(columns='Muon_genPdgId')
+
+	
+	pt = data['Muon_pt']
+#	data = data.drop(columns='Muon_pt')
+
+	target = target.map(label_dict)
+	data = (data-data.mean())/data.std()
+
+	return data, target
 
 class DATA:
 
-	def __init__(self,fname,name):
+	def __init__(self,fname,name, model_vars):
 		self.name = name
 		self.treeName = 'Events'
 		self.fname = fname
 		# tmp = root_numpy.root2array(self.fname,self.treeName)
-		file = uproot.open(self.fname)
-		events = file[self.treeName]
+		events = uproot.open(self.fname)[self.treeName]
 		# data = pd.DataFrame(tmp)
 		
 
 		#### using uproot to chunk mu and gen data ####
-		# startTot = time.process_time()
-		pdgIds = np.array([])
+		dfs = np.array([])
 		memChunks = [i for i in events['GenPart_pdgId'].mempartitions(1e6)] #read 1 MB at a time
+		print("# memChunks:",len(memChunks))
 		for i in memChunks:
 			memStart = i[0]
 			memStop = i[1]
-			genData = events.array('GenPart_pdgId',entrystart=memStart,entrystop=memStop)
-			dataMu = events.array('Muon_genPartIdx',entrystart=memStart,entrystop=memStop)
-			# startChunk = time.process_time()
-			pdgIds = np.append(pdgIds,[-999 if mu == -1 else genData[i][mu] for i, idxs in enumerate(dataMu) for j, mu in enumerate(idxs)])
-			# stopChunk = time.process_time()
-			# print("chunk time",stopChunk-startChunk,"secs")
+			genData = events.array('GenPart_pdgId',entrystart=memStart,entrystop=memStart)
+			dataMu = events.array('Muon_genPartIdx',entrystart=memStop,entrystop=memStop)
+			pdgIds = np.array([-999 if mu == -1 else genData[i][mu] for i, idxs in enumerate(dataMu) for j, mu in enumerate(idxs)])
+			pdgIds = pdgIds.flatten()
+			data = events.pandas.df(model_vars,entrystart=genMemStart,entrystop=genMemStop)
+			data['Muon_genPdgId'] = pdgIds
+			dfs = np.append(dfs,data)
 
-		pdgIds = pdgIds.flatten()
+		
 		# stopTot = time.process_time()
 		# print("total time",stopTot-startTot,"secs")
 		# print(pdgIds.shape)
@@ -137,21 +168,19 @@ class DATA:
 		
 # 
 		# data = expandList(data, expCols)
-		data = events.pandas.df('Muon_*')
-		data['Muon_genPdgId'] = pdgIds
 
-
-	
+		
+		data = pd.concat(dfs)
 		self.data1 = data[abs(data.Muon_genPdgId) == 13]
 		self.data2 = data[data.Muon_genPdgId == -999]
 		self.data3 = data[abs(data.Muon_genPdgId) == 11]
 		self.data4 = data[abs(data.Muon_genPdgId) == 211]
 		self.data5 = data[abs(data.Muon_genPdgId) == 321]
 		self.data6 = data[abs(data.Muon_genPdgId) == 2212]
-		
+			
 
-		self.datacoll = {'mu': self.data1, 'U':self.data2, 'e':self.data3, 'pi':self.data4, 'k':self.data5,'p':self.data6}
-		
+		self.datacoll = np.append(self.datacoll,{'mu': self.data1, 'U':self.data2, 'e':self.data3, 'pi':self.data4, 'k':self.data5,'p':self.data6})
+		return dfs
 	
 	def __del__(self):
 		del self.data1
@@ -162,6 +191,7 @@ class DATA:
 		del self.data6
 			
 	def report(self):
+		data = pd.concat(dfs)
 		print("Report for data "+self.name)
 		print("mu", self.data1.shape)
 		print("unmatched", self.data2.shape)
