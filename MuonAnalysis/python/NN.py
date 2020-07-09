989d8ad6d1b1b38a1733db49591e46fcd83020ae
Margaret Lazarovits <mlazarovits@gmail.com>
2020-07-08 20:30:07 -0500

    debug

--- MuonAnalysis/python/NN.py
+++ MuonAnalysis/python/NN.py
@@ -62,8 +62,10 @@ def evaluateModel(model_y, true_y, model_pt, fname, tag, path, results=None ):
 
 	# begin the counting 
 	for i , (my, ty, pt) in enumerate(zip(pred, true_y, model_pt)):
+		ty = np.array([ty])
 		if i < 10:
 			print(i,my,ty,pt)
+
 			print(ty.shape)
 			print(np.flatnonzero(ty == 1))
 		# labelidx = -1
