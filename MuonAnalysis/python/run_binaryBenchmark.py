1348c6fd202bdb8742e4a1f75749b2dfed2b8b9d
Margaret Lazarovits <mlazarovits@gmail.com>
2020-07-11 12:17:51 -0500

    debug

--- MuonAnalysis/python/run_binaryBenchmark.py
+++ MuonAnalysis/python/run_binaryBenchmark.py
@@ -138,6 +138,8 @@ dy_x, dy_y, dy_pt = prepareSet(dyTrain,mdict)
 for i, ty in enumerate(dy_y):
 	if ty.shape != (2,):
 		print(i,ty)
+
+print(dyTrain['Muon_genPdgId'].iloc[4820])
 # print(np.argwhere(np.isnan(tt_y)))
 # print(np.argwhere(np.isnan(qcd_y)))
 # print(np.argwhere(np.isnan(comb_y)))
